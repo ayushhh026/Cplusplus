63c19d92cbf042f91a6325393dfa012c3ec9d4c2
ayushhh26 <ayushshetty26@gmail.com>
2026-02-01 20:27:10 +0530

    data types

---
+++ 3Datatype.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age =5;
+    cout<<age<<"\n";
+    string name = "Helloworld";
+    cout<<name<<endl;
+    char gender = 'm';
+    cout<<gender<<endl;
+    bool isAlive = true;
+    cout<<isAlive<<"\n";
+    float weight = 100.982;
+    cout<<weight<<"\n";
+    cout<<sizeof(int);
+    return 0;
+}
