6e1a0ba621431849f1bacd0a15681f9c27a8f2d8
ayushhh26 <ayushshetty26@gmail.com>
2026-02-08 21:09:06 +0530

    operator types

---
+++ 6Operators.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter 2 numbers for add subtract multiply divide"<<endl;
+    cin>>a>>b;
+    cout<<"Addition is "<<(a+b)<<endl;
+    cout<<"Subtraction is "<<(a-b)<<endl;
+    cout<<"Multiplication is "<<(a*b)<<endl;
+    cout<<"Divison is "<<(a/b)<<endl;
+}
