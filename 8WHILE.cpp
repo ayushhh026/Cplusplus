6240507ef7cdab6209fb72566a9988da552a3a02
ayushhh26 <ayushshetty26@gmail.com>
2026-02-09 21:23:53 +0530

    Looping

---
+++ 8WHILE.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,i=0;
+    cout<<"Enter a number to check : ";
+    cin>>n;
+    while (i<n)
+    {
+        cout<<"GFG\n";
+        i++;
+    }
+    return 0;
+}
