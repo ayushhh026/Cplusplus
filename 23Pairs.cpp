15e5f0983220f3c455a1b479955e92a080e332b6
ayushhh26 <ayushshetty26@gmail.com>
2026-03-04 17:58:22 +0530

    STL pairs

---
+++ 23Pairs.cpp
@@ -0,0 +1,11 @@
+#include<utility>
+#include<iostream>
+using namespace std;
+int main()
+{
+    pair<int,int> p1(10,20);
+    pair<int,string> p2(10,"Hello");
+    cout<<p1.first<<" "<<p1.second<<endl;
+    cout<<p2.first<<" "<<p2.second<<endl;
+    return 0;
+}
