3494b649d59a5dcf3034f580dffd56ecab890f1d
ayushhh26 <ayushshetty26@gmail.com>
2026-03-18 19:32:42 +0530

    Unordered Map

---
+++ 36UnorderedMap.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+int main()
+{
+    unordered_map<string,int>m;
+    m["gfg"]=20;
+    m["ide"]=30;
+    m.insert({"Course",15});
+    for(auto x:m)
+        cout<<x.first<<" "<<x.second<<endl;
+        return 0;
+        
+}
